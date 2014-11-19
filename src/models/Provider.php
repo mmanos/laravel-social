7b9ee48a54d2058824cad161fd5c8e485fe87566
Randy Glenn Aguirre <rgat.randy@gmail.com>
2014-11-19 10:43:47 +0800

    deleted trailing spaces

--- src/models/Provider.php
+++ src/models/Provider.php
@@ -10,28 +10,28 @@ class Provider extends Model
 {
 	protected $hidden = array('user_id', 'updated_at');
 	protected $guarded = array('id');
-	
+
 	public function getTable()
 	{
 		return Config::get('laravel-social::table', 'user_providers');
 	}
-	
+
 	public function getAccessTokenAttribute($value)
 	{
 		return json_decode(Crypt::decrypt($value), true);
 	}
-	
+
 	public function setAccessTokenAttribute($value)
 	{
 		$this->attributes['access_token'] = Crypt::encrypt(json_encode($value));
 	}
-	
+
 	public function request($path, $method = 'GET', $body = null, array $extra_headers = array())
 	{
 		$access_token = $this->access_token;
-		
+
 		$service = Facades\Social::service($this->provider);
-		
+
 		if (2 === Facades\Social::oauthSpec($this->provider)) {
 			$token = new StdOAuth2Token;
 			$token->setAccessToken(array_get($access_token, 'token'));
@@ -41,26 +41,26 @@ class Provider extends Model
 			$token->setAccessToken(array_get($access_token, 'token'));
 			$token->setAccessTokenSecret(array_get($access_token, 'secret'));
 		}
-		
+
 		$service->getStorage()->storeAccessToken(ucfirst($this->provider), $token);
-		
+
 		try {
 			return $service->request($path, $method, $body, $extra_headers);
 		} catch (\OAuth\Common\Http\Exception\TokenResponseException $e) {
 			if ($this->refreshAccessToken()) {
 				return $service->request($path, $method, $body, $extra_headers);
 			}
-			
+
 			throw $e;
 		}
 	}
-	
+
 	public function refreshAccessToken()
 	{
 		$access_token = $this->access_token;
-		
+
 		$service = Facades\Social::service($this->provider);
-		
+
 		if (2 === Facades\Social::oauthSpec($this->provider)) {
 			$token = new StdOAuth2Token;
 			$token->setAccessToken(array_get($access_token, 'token'));
@@ -72,19 +72,19 @@ class Provider extends Model
 			$token->setAccessTokenSecret(array_get($access_token, 'secret'));
 			$token->setRefreshToken(array_get($access_token, 'refresh_token'));
 		}
-		
+
 		$service->getStorage()->storeAccessToken(ucfirst($this->provider), $token);
-		
+
 		try {
 			$new_token = $service->refreshAccessToken($token);
 		} catch (\Exception $e) {
 			return false;
 		}
-		
+
 		if (!$new_token->getAccessToken()) {
 			return false;
 		}
-		
+
 		$access_token['token'] = $new_token->getAccessToken();
 		if ($new_token->getEndOfLife()) {
 			$access_token['end_of_life'] = $new_token->getEndOfLife();
@@ -95,10 +95,10 @@ class Provider extends Model
 		if (2 !== Facades\Social::oauthSpec($this->provider) && $new_token->getAccessTokenSecret()) {
 			$access_token['secret'] = $new_token->getAccessTokenSecret();
 		}
-		
+
 		$this->access_token = $access_token;
 		$this->save();
-		
+
 		return true;
 	}
 }
--- src/views/social/complete.blade.php
+++ src/views/social/complete.blade.php
@@ -2,23 +2,23 @@
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
 			<h3>Please complete your profile...</h3>
-			
+
 			{{ Form::open(array('action' => 'Mmanos\Social\SocialController@postComplete')) }}
 				<fieldset>
 					@foreach ($failed_fields as $idx => $field)
 						<div class="form-group{{ $errors->has($field) ? ' has-error' : '' }}">
 							{{ Form::label($field, ucwords(str_replace(array('-', '_'), ' ', $field))) }}
-							
+
 							@if ($idx === 0)
 								{{ Form::text($field, Input::old($field, array_get($info, $field)), array('autofocus' => 'autofocus', 'class' => 'form-control')) }}
 							@else
 								{{ Form::text($field, Input::old($field, array_get($info, $field)), array('class' => 'form-control')) }}
 							@endif
-							
+
 							{{ $errors->first($field, '<span class="help-block">:message</span>') }}
 						</div>
 					@endforeach
-					
+
 					<div class="form-group">
 						{{ Form::submit('Save', array('class' => 'btn btn-primary')) }}
 					</div>
